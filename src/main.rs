362c4cf2f3b2eba0d59480350b10944461a09fde
lbeder <lbeder@users.noreply.github.com>
2018-12-29 02:59:43 +0200

    Get secret + refactor

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,19 @@ use std::path::Path;
 use std::process::exit;
 
 static VERSION: &'static str = "0.1.0";
-static DEFAULT_ITERATIONS: usize = 100;
-static DEFAULT_N: usize = 1048576;
-static DEFAULT_R: usize = 8;
-static DEFAULT_P: usize = 1;
+static DEFAULT_ITERATIONS: u32 = 100;
+static DEFAULT_N: u32 = 1048576;
+static DEFAULT_R: u32 = 8;
+static DEFAULT_P: u32 = 1;
+
+#[derive(Debug)]
+struct ScryptKDFOptions {
+    n: u32,
+    r: u32,
+    p: u32,
+    iterations: u32,
+    salt: String
+}
 
 fn print_usage(program: &str, opts: Options) {
     let brief = format!("Usage: {} v{} [options]", program, VERSION);
@@ -19,10 +28,7 @@ fn print_version(program: &str) {
     print!("{} v{}", program, VERSION);
 }
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let program = Path::new(&args[0]).file_name().unwrap().to_str().unwrap();
-
+fn get_options() -> Options {
     let mut opts = Options::new();
     opts.optopt("i", "iterations", "set the number of required iterations", "ITER");
     opts.optopt("s", "salt", "set the salt to protect against Rainbow table attacks", "SALT");
@@ -32,6 +38,14 @@ fn main() {
     opts.optflag("h", "help", "print this help menu");
     opts.optflag("v", "version", "print version information");
 
+    opts
+}
+
+fn parse_options() -> ScryptKDFOptions {
+    let opts = get_options();
+    let args: Vec<String> = env::args().collect();
+    let program = Path::new(&args[0]).file_name().unwrap().to_str().unwrap();
+
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => { m }
         Err(f) => { panic!(f.to_string()) }
@@ -39,16 +53,16 @@ fn main() {
 
     if matches.opt_present("v") {
         print_version(&program);
-        return;
+        exit(-1);
     }
 
     if matches.opt_present("h") {
         print_usage(&program, opts);
-        return;
+        exit(-1);
     }
 
     let iterations = matches.opt_str("i")
-        .and_then(|o| o.parse::<usize>().ok())
+        .and_then(|o| o.parse::<u32>().ok())
         .unwrap_or(DEFAULT_ITERATIONS);
     let salt = match matches.opt_str("s") {
         Some(s) => s,
@@ -58,14 +72,40 @@ fn main() {
         }
     };
     let n = matches.opt_str("n")
-        .and_then(|o| o.parse::<usize>().ok())
+        .and_then(|o| o.parse::<u32>().ok())
         .unwrap_or(DEFAULT_N);
     let r = matches.opt_str("r")
-        .and_then(|o| o.parse::<usize>().ok())
+        .and_then(|o| o.parse::<u32>().ok())
         .unwrap_or(DEFAULT_R);
     let p = matches.opt_str("p")
-        .and_then(|o| o.parse::<usize>().ok())
+        .and_then(|o| o.parse::<u32>().ok())
         .unwrap_or(DEFAULT_P);
 
-    println!("Working with parameters: n={}, r={}, p={}, iterations={}", n, r, p, iterations);
+    ScryptKDFOptions {
+        n: n,
+        r: r,
+        p: p,
+        iterations: iterations,
+        salt: salt
+    }
+}
+
+fn get_secret() -> String {
+    let pass = rpassword::prompt_password_stdout("Enter your secret: ").unwrap();
+    let pass2 = rpassword::prompt_password_stdout("Enter your secret again: ").unwrap();
+
+    if pass != pass2 {
+        println!("Secrets don't match!");
+        exit(-1);
+    }
+
+    String::from(pass)
+}
+
+fn main() {
+    let opts = parse_options();
+    println!("Working with {:?}", opts);
+
+    let secret = get_secret();
+    println!("Your secret is {}", secret);
 }

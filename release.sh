0fa043f0b3e7efe80b0cd57425c3d57502893058
Leonid Beder <leonid.beder@gmail.com>
2019-01-12 12:35:07 +0200

    Only sign the hash and add the signatures to the release notes

--- release.sh
+++ release.sh
@@ -2,18 +2,24 @@
 
 VERSION=$(cargo pkgid | cut -d# -f2 | cut -d: -f2)
 
+rm -rf target/*.tgz target/*.tgz.asc target/release.md
+
 echo "Building v${VERSION} for Mac OS..."
 APPLE_RELEASE="target/scrypt-kdf-${VERSION}-osx.tgz"
 cargo build --release --target=x86_64-apple-darwin
 tar zcvf ${APPLE_RELEASE} target/x86_64-apple-darwin/release/scrypt-kdf
+APPLE_RELEASE_SHA512=$(shasum -a512 ${APPLE_RELEASE})
+APPLE_RELEASE_ASC=${APPLE_RELEASE}.asc
 
 echo "Building v${VERSION} for Linux AMD64..."
 LINUX_RELEASE="target/scrypt-kdf-${VERSION}-linux-amd64.tgz"
 CROSS_COMPILE=x86_64-linux-musl- cargo build --release --target=x86_64-unknown-linux-musl
 tar zcvf ${LINUX_RELEASE} target/x86_64-unknown-linux-musl/release/scrypt-kdf
+LINUX_RELEASE_SHA512=$(shasum -a512 ${LINUX_RELEASE})
+LINUX_RELEASE_ASC=${LINUX_RELEASE}.asc
 
-keybase pgp sign -i ${APPLE_RELEASE} --detached > ${APPLE_RELEASE}.asc
-keybase pgp sign -i ${LINUX_RELEASE} --detached > ${LINUX_RELEASE}.asc
+keybase pgp sign --clearsign -m "${APPLE_RELEASE_SHA512}" > ${APPLE_RELEASE_ASC}
+keybase pgp sign --clearsign -m "${LINUX_RELEASE_SHA512}" > ${LINUX_RELEASE_ASC}
 
 RELEASE_NOTES="target/release.md"
 echo "Preparing release notes..."
@@ -23,13 +29,25 @@ cat <<EOF >$RELEASE_NOTES
 
 ## Mac OS
 
+### SHA512
+
 \`\`\`bash
-shasum -a512 ${APPLE_RELEASE} $(shasum -a512 ${APPLE_RELEASE})
+shasum -a512 ${APPLE_RELEASE} ${APPLE_RELEASE_SHA512}
 \`\`\`
+
+### Digital Signature
+
+$(cat ${APPLE_RELEASE_ASC})
 
 ## Linux AMD64
 
+### SHA512
+
 \`\`\`bash
-shasum -a512 ${LINUX_RELEASE} $(shasum -a512 ${LINUX_RELEASE})
+shasum -a512 ${LINUX_RELEASE} ${LINUX_RELEASE_SHA512}
 \`\`\`
+
+### Digital Signature
+
+$(cat ${LINUX_RELEASE_ASC})
 EOF
